2e9e5524a5d9317b05ef879e4c3a3cb2375b43e1
srvr-2 <srvr-2@mtk.local>
2018-12-24 16:13:34 +0300

    Init commit

---
+++ BoardF/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardF
+{    
+    public class Game
+    {
+        int size = 0;
+
+        public Game (int size)
+        {
+            this.size = size;
+        }
+
+        public void Start (int seed = 0)
+        {
+
+        }
+
+        public int PressAt(int x, int y)
+        {
+            return 0;
+        }
+
+        public int GetDigitAt(int x, int y)
+        {
+            return 0;
+        }
+
+        public bool Solved()
+        {
+            return false;
+        }
+    }
+}
