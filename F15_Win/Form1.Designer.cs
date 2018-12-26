189de8b674570de974f8d48e8f8385d80f97f627
AS <wimon@mail.ru>
2018-12-26 13:56:56 +0300

    Add win forms project, create simple win form with buttons

---
+++ F15_Win/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace F15_Win
+{
+    partial class FormF15
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormF15));
+            this.b00 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b20 = new System.Windows.Forms.Button();
+            this.b30 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b01 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b02 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b03 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b00
+            // 
+            this.b00.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b00.Location = new System.Drawing.Point(12, 12);
+            this.b00.Name = "b00";
+            this.b00.Size = new System.Drawing.Size(72, 69);
+            this.b00.TabIndex = 0;
+            this.b00.TabStop = false;
+            this.b00.Text = "F";
+            this.b00.UseVisualStyleBackColor = true;
+            this.b00.Click += new System.EventHandler(this.b00_Click);
+            // 
+            // b10
+            // 
+            this.b10.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b10.Location = new System.Drawing.Point(90, 12);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(72, 69);
+            this.b10.TabIndex = 1;
+            this.b10.TabStop = false;
+            this.b10.Text = "F";
+            this.b10.UseVisualStyleBackColor = true;
+            // 
+            // b20
+            // 
+            this.b20.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b20.Location = new System.Drawing.Point(168, 12);
+            this.b20.Name = "b20";
+            this.b20.Size = new System.Drawing.Size(72, 69);
+            this.b20.TabIndex = 2;
+            this.b20.TabStop = false;
+            this.b20.Text = "F";
+            this.b20.UseVisualStyleBackColor = true;
+            // 
+            // b30
+            // 
+            this.b30.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b30.Location = new System.Drawing.Point(247, 12);
+            this.b30.Name = "b30";
+            this.b30.Size = new System.Drawing.Size(72, 69);
+            this.b30.TabIndex = 3;
+            this.b30.TabStop = false;
+            this.b30.Text = "F";
+            this.b30.UseVisualStyleBackColor = true;
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b31.Location = new System.Drawing.Point(247, 87);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(72, 69);
+            this.b31.TabIndex = 7;
+            this.b31.TabStop = false;
+            this.b31.Text = "F";
+            this.b31.UseVisualStyleBackColor = true;
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b21.Location = new System.Drawing.Point(168, 87);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(72, 69);
+            this.b21.TabIndex = 6;
+            this.b21.TabStop = false;
+            this.b21.Text = "F";
+            this.b21.UseVisualStyleBackColor = true;
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b11.Location = new System.Drawing.Point(90, 87);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(72, 69);
+            this.b11.TabIndex = 5;
+            this.b11.TabStop = false;
+            this.b11.Text = "F";
+            this.b11.UseVisualStyleBackColor = true;
+            // 
+            // b01
+            // 
+            this.b01.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b01.Location = new System.Drawing.Point(12, 87);
+            this.b01.Name = "b01";
+            this.b01.Size = new System.Drawing.Size(72, 69);
+            this.b01.TabIndex = 4;
+            this.b01.TabStop = false;
+            this.b01.Text = "F";
+            this.b01.UseVisualStyleBackColor = true;
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b32.Location = new System.Drawing.Point(247, 165);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(72, 69);
+            this.b32.TabIndex = 11;
+            this.b32.TabStop = false;
+            this.b32.Text = "F";
+            this.b32.UseVisualStyleBackColor = true;
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b22.Location = new System.Drawing.Point(168, 165);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(72, 69);
+            this.b22.TabIndex = 10;
+            this.b22.TabStop = false;
+            this.b22.Text = "F";
+            this.b22.UseVisualStyleBackColor = true;
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b12.Location = new System.Drawing.Point(90, 165);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(72, 69);
+            this.b12.TabIndex = 9;
+            this.b12.TabStop = false;
+            this.b12.Text = "F";
+            this.b12.UseVisualStyleBackColor = true;
+            // 
+            // b02
+            // 
+            this.b02.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b02.Location = new System.Drawing.Point(12, 165);
+            this.b02.Name = "b02";
+            this.b02.Size = new System.Drawing.Size(72, 69);
+            this.b02.TabIndex = 8;
+            this.b02.TabStop = false;
+            this.b02.Text = "F";
+            this.b02.UseVisualStyleBackColor = true;
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b33.Location = new System.Drawing.Point(247, 240);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(72, 69);
+            this.b33.TabIndex = 15;
+            this.b33.TabStop = false;
+            this.b33.Text = "F";
+            this.b33.UseVisualStyleBackColor = true;
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b23.Location = new System.Drawing.Point(168, 240);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(72, 69);
+            this.b23.TabIndex = 14;
+            this.b23.TabStop = false;
+            this.b23.Text = "F";
+            this.b23.UseVisualStyleBackColor = true;
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b13.Location = new System.Drawing.Point(90, 240);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(72, 69);
+            this.b13.TabIndex = 13;
+            this.b13.TabStop = false;
+            this.b13.Text = "F";
+            this.b13.UseVisualStyleBackColor = true;
+            // 
+            // b03
+            // 
+            this.b03.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b03.Location = new System.Drawing.Point(12, 240);
+            this.b03.Name = "b03";
+            this.b03.Size = new System.Drawing.Size(72, 69);
+            this.b03.TabIndex = 12;
+            this.b03.TabStop = false;
+            this.b03.Text = "F";
+            this.b03.UseVisualStyleBackColor = true;
+            // 
+            // FormF15
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 324);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b03);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b02);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b01);
+            this.Controls.Add(this.b30);
+            this.Controls.Add(this.b20);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b00);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormF15";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "F.15";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b00;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b20;
+        private System.Windows.Forms.Button b30;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b01;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b02;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b03;
+    }
+}
+
---
+++ F15_Win/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F15_Win
+{
+    public partial class FormF15 : Form
+    {
+        public FormF15()
+        {
+            InitializeComponent();
+        }
+
+        private void b00_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
